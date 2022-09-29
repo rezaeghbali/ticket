e36a77b6d8f3f06b624445b0957afcecb269d153
reza eghbali <r.eghbali199@gmail.com>
2022-09-29 13:46:14 +0330

    commit 1

---
+++ js/script.js
@@ -0,0 +1,26 @@
+﻿
+let countrySelect=document.querySelector('.countrySelect')
+let citySelect=document.querySelector('.citySelect')
+let cityObject={
+    Iran: ['tehran','tabriz','shiraz'],
+    Turkey: ['ankara','izmir','adana'],
+    US: ['shikago','newyork','menfis']
+}
+countrySelect.addEventListener('change',function () {
+    if(countrySelect.value==='Please Select'){
+        citySelect.innerHTML=''
+        citySelect.innerHTML+='<option>Select City</option>'
+    }else{
+        let countryInputValue=countrySelect.value;
+        let cityOfCountry=cityObject[countryInputValue]
+        citySelect.innerHTML=''
+        cityOfCountry.forEach(function (city) {
+            citySelect.innerHTML+='<option>'+city+'</option>'
+        })
+    }
+})
+
+
+
+
+
